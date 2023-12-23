fedf80a901bcc31e8660d8fa7a49a45952c97dbe
Artemi_grou@mail.ru <Artemi_grou@mail.ru>
2023-12-23 14:06:15 +1000

    masterka

---
+++ Examen/Learn.cs
@@ -0,0 +1,56 @@
+﻿using Examen.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    internal class Learn
+
+    {
+        //1 Проверка на наличие БД.
+
+        //static Rey_InterpriseContext instance;
+        //public static Rey_InterpriseContext GetInstance()
+        //{
+        //    if (instance == null)
+        //        instance = new Rey_InterpriseContext();
+        //    return instance;
+        //}
+
+
+
+
+        //2 Название интерфейса для BaseTool и CommandVM.
+
+        //INotifyPropertyChanged
+
+        //ICommand
+
+
+
+        //3 Содержимое CommandVM.
+
+        /* public event EventHandler? CanExecuteChanged;
+
+        Action action;
+
+        public CommandVM(Action action)
+        {
+            this.action = action;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            action();
+        } */
+
+    }
+}
